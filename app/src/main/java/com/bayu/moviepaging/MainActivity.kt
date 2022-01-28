a2dd9e2a67cda273f52d25a57d0f40d8eb45bf62
bayu07750 <bayu07750@gmail.com>
2022-01-28 14:07:38 +0700

    setup navigation bar

--- app/src/main/java/com/bayu/moviepaging/MainActivity.kt
+++ app/src/main/java/com/bayu/moviepaging/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.bayu.moviepaging
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.bayu.moviepaging.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = binding.fragmentContainerView.getFragment<NavHostFragment>()
+        navController = navHostFragment.navController
+
+        setupSmoothBottomMenu()
+    }
+
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
     }
 }
---
+++ app/src/main/java/com/bayu/moviepaging/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,29 @@
+package com.bayu.moviepaging.ui.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentFactory
+import com.bayu.moviepaging.R
+import com.bayu.moviepaging.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
