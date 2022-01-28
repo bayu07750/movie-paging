215544f9d04c82d59e0fd8a6e35928cfe4c6cce3
bayu07750 <bayu07750@gmail.com>
2022-01-28 15:23:03 +0700

    setup home

--- app/src/main/java/com/bayu/moviepaging/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bayu/moviepaging/ui/home/HomeFragment.kt
@@ -1,17 +1,22 @@
 package com.bayu.moviepaging.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.bayu.moviepaging.R
 import com.bayu.moviepaging.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val spinnerItems = listOf("All", "Movie", "Tv", "Person")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +26,30 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        with(binding.spinner) {
+            val spinnerAdapter = ArrayAdapter(requireContext(), R.layout.layout_spinner, spinnerItems)
+            adapter = spinnerAdapter
+            onItemSelectedListener = this@HomeFragment
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        Toast.makeText(requireContext(), spinnerItems[position], Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
     }
 }
